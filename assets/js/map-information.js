87bb02250d0dfdcc08c80db6b99a21b81f8eaf6d
Rupert <jackie.rupert.hewson@gmail.com>
2019-08-07 16:45:05 +0000

    user input

---
+++ assets/js/map-information.js
@@ -0,0 +1,10 @@
+function fetchGoogleInformation(event) {
+    
+    var place =$("beach-input").val();
+    if (!place) {
+        $("#gm-user-data").html(`<h2>Please enter location</h2>`);
+    return;
+}
+    
+}
+
